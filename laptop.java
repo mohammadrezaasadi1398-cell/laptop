db38a8b5cd36d8653cf89aef2e06e022d5b7a2ca
mohammadreza asadi <mohammadreza.asadi1398@gmail.com>
2026-09-23 21:33:54 +0200

    eigenschaften hinzugefugt

--- laptop.java
+++ laptop.java
@@ -3,4 +3,41 @@ public class laptop
     private String hersteller;
     private int kosten;
     private boolean neu;
+    
+    
+    public laptop(String neuHersteller,int neukosten,boolean neuNeu)
+    {
+        setHersteller(neuHersteller);
+        setKosten(neukosten);
+        setNeu(neuNeu);
+        
+    }
+    
+    public laptop(String neuHersteller,boolean neuNeu)
+    {
+        setHersteller(neuHersteller);
+        setKosten(1000);
+        setNeu(neuNeu);
+    }
+    
+    public laptop()
+    {
+        setHersteller("lenovo");
+        setKosten(1000);
+        setNeu(true);
+    }
+    
+    public void setHersteller(String neuHersteller)
+    {
+        hersteller=neuHersteller;
+    }
+    public void setKosten(int neuKosten)
+    {
+        kosten=neuKosten;
+    }
+    public void setNeu(boolean neuNeu)
+    {
+        neu=neuNeu;
+    }
+    
 }
